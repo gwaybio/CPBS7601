d11f513268d109472dec5285d2217f96e2673b9d
Gregory Way <gregory.way@gmail.com>
2024-08-23 12:23:31 -0600

    Adding workflow tutorial (#5)
    * add papermill
    
    * add example bash workflow

---
+++ lectures/5.workflows/example_bash_workflow.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# We are going to demonstrate a simple bash workflow
+# We will execute the pandas data wrangling notebook, with a single command
+
+# Step 1: Navigate into the 4.data_wrangling directory
+cd ../4.data_wrangling
+
+# Step 2: Confirm that you're using the correct conda environment
+# Note: You must first have created the environment (see lectures/3.environments/)
+source "$(conda info --base)/etc/profile.d/conda.sh"
+conda activate cpbs7601
+
+# Step 3: Execute the pandas example
+papermill pandas_example.ipynb /dev/null
